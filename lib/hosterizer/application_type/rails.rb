fe2e3ab9b1223e749a8f8f09d58ae007d67344af
Misha Gorodnitzky <misaka@pobox.com>
2010-02-13 16:32:37 +0000

    Added getting info for each application.
    - Parseing of database.yml files for Rails apps.

--- lib/hosterizer/application_type/rails.rb
+++ lib/hosterizer/application_type/rails.rb
@@ -5,8 +5,14 @@ module Hosterizer::ApplicationType::Rails
   extend Hosterizer::ApplicationType
 
   def self.reject_paths
-    @@reject_paths ||= []
+    @@reject_paths ||= [ /\.\d+$/ ]
     @@reject_paths
+  end
+
+  def self.options
+    @options ||= {
+      :environment => 'production'
+    }
   end
 
   def self.find_applications( ssh, host )
@@ -28,6 +34,20 @@ module Hosterizer::ApplicationType::Rails
     end
 
     applications
+  end
+
+  def self.get_app_details( app, info, connection )
+    app_details = {}
+
+    cmd = "cat /opt/railsapps/applications/#{app}/current/config/database.yml"
+    connection.exec!( cmd ) do |ch, stream, data|
+      if stream == :stdout
+        yaml = YAML.load( data )
+        # Parse the yaml doc for the environment ... er, which one? 'production'?
+        info[:db_type] = yaml[ options[:environment] ]['adapter']
+        info[:db_host] = yaml[ options[:environment] ]['host']
+      end
+    end
   end
 end
 
